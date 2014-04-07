c817e9faceac6b423293bac6f7699965718231cb
Joe Dou <joe.dou@gmail.com>
2014-04-06 18:19:47 -0700

    recursion precourse

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,26 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var output = [];
+
+  var getChildNode = function (htmlNode){
+  	var child = htmlNode.childNodes;
+
+  	for (var i =0; i < child.length; i++){
+  		if (child[i].hasOwnProperty("classList")) {
+  			if(child[i].classList.contains(className)){
+  				output.push(child[i]);
+  			}
+  		}
+  		if (child[i].childNodes.length > 0)
+  		{
+  			getChildNode(child[i]);
+  		}
+  	}  	
+  }
+
+  getChildNode(document.body);
+
+  return output;
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,48 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  // your code goes here  
+  if ((typeof(obj) == "function") || (typeof(obj) == "undefined")){
+    return undefined;
+  }
+
+  if (obj == null)
+  {
+    return "null";
+  }
+  else if (typeof(obj) == "string"){
+    return "\""+obj+"\"";
+  }else if(Array.isArray(obj)){
+    var tempObj = obj;
+    var output = "[]";
+
+    for (var i = 0; i < obj.length; i++){
+      var temp = tempObj[i];
+      var tempString = stringifyJSON(temp);
+      if (i == obj.length-1){
+        var newOutput = output.slice(0,-1)+tempString.toString()+output.slice(-1);
+      }else{
+        var newOutput = output.slice(0,-1)+tempString.toString()+","+output.slice(-1);
+      }
+      output = newOutput;
+    }
+    return (output);
+  }else if(typeof(obj) == "object"){
+    var tempObj = obj;
+    var output = "{}";
+    for (var key in obj){
+      var tempKey = stringifyJSON(key);
+      var tempString = stringifyJSON(obj[key]);
+      if (tempString != undefined){
+        var output = output.slice(0,-1)+tempKey+":"+tempString+","+output.slice(-1);
+      }
+    }
+    if (output.slice(-2,-1) == ","){
+      output = output.slice(0,-2) + output.slice(-1);
+    }
+    return(output);
+
+  }else{
+  	return obj.toString();
+  }
 };
